157ebdf40683e3857ddf436e34ac678a5b05f49c
simrin051 <simrinjoshi@yahoo.com>
2022-04-23 15:35:18 +0530

    video detail

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -7,8 +7,18 @@ import { v4 as uuid } from "uuid";
 export const videos = [
   {
     _id: uuid(),
-    title: "DSA - Algorithms Introduction",
-    url: "https://www.youtube.com/watch?v=UxzycIJA5os",
+    title: "Listening",
+    url: "https://www.youtube.com/embed/7wUCyjiyXdg",
+    channel: "Gatebook video lectures",
+    categoryName: 'DSA',
+    likes: '2000',
+    uploadTime: '2 weeks',
+    thumbNail: 'https://yt3.ggpht.com/ytc/AKedOLTlEpryCbdrrlX-1Gjr_L_bUAvMx_b-8EIXHoRGXA=s88-c-k-c0x00ffffff-no-rj'
+  },
+  {
+    _id: uuid(),
+    title: "Listening",
+    url: "https://www.youtube.com/embed/ezZ-EJm9IRY",
     channel: "Gatebook video lectures",
     categoryName: 'DSA',
     likes: '2000',
@@ -17,33 +27,23 @@ export const videos = [
   },
   {
     _id: uuid(),
-    title: "DSA - Algorithms Introduction",
-    url: "https://www.youtube.com/watch?v=UxzycIJA5os",
+    title: "Listening",
+    url: "https://www.youtube.com/embed/x-gpFgtXCF4",
     channel: "Gatebook video lectures",
     categoryName: 'DSA',
     likes: '2000',
     uploadTime: '2 weeks',
-    thumbNail: 'https://yt3.ggpht.com/ytc/AKedOLSixIWOOe7bcgzIWkSpr2K5PmBVlW5agwgCXJ2xyw=s48-c-k-c0x00ffffff-no-rj'
+    thumbNail: 'https://yt3.ggpht.com/ytc/AKedOLSFa9q2TbaTVodMhIHmAKof3VtplzXoy4UTeFYbbA=s48-c-k-c0x00ffffff-no-rj'
   },
   {
     _id: uuid(),
     title: "DSA - Algorithms Introduction",
-    url: "https://www.youtube.com/watch?v=UxzycIJA5os",
+    url: "https://www.youtube.com/embed/W59Abl7kcp0",
     channel: "Gatebook video lectures",
     categoryName: 'DSA',
     likes: '2000',
     uploadTime: '2 weeks',
-    thumbNail: 'https://yt3.ggpht.com/ytc/AKedOLSixIWOOe7bcgzIWkSpr2K5PmBVlW5agwgCXJ2xyw=s48-c-k-c0x00ffffff-no-rj'
-  },
-  {
-    _id: uuid(),
-    title: "DSA - Algorithms Introduction",
-    url: "https://www.youtube.com/watch?v=UxzycIJA5os",
-    channel: "Gatebook video lectures",
-    categoryName: 'DSA',
-    likes: '2000',
-    uploadTime: '2 weeks',
-    thumbNail: 'https://yt3.ggpht.com/ytc/AKedOLSixIWOOe7bcgzIWkSpr2K5PmBVlW5agwgCXJ2xyw=s48-c-k-c0x00ffffff-no-rj'
+    thumbNail: 'https://yt3.ggpht.com/ytc/AKedOLRSYsU5M2XZGBjjln-t46KO-AS5ikFRWCWMr87R=s48-c-k-c0x00ffffff-no-rj'
   },
   {
     _id: uuid(),
